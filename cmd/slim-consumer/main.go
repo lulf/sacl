d26240262410cfea071f7f12de4005571bf32b8a
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2020-01-26 22:22:14 +0100

    FIx consumer test

--- cmd/slim-consumer/main.go
+++ cmd/slim-consumer/main.go
@@ -23,15 +23,17 @@ func main() {
 	var connectHost string
 	var topic string
 	var port int
+	var numMessages int
 
 	flag.Int64Var(&offset, "o", 5, "Offset to start consuming from")
 	flag.StringVar(&connectHost, "c", "127.0.0.1", "Host to connect to")
 	flag.StringVar(&topic, "t", "mytopic", "Topic to consume from")
+	flag.IntVar(&numMessages, "m", -1, "Number of messages to receive")
 	flag.IntVar(&port, "p", 5672, "Port to connect to")
 
 	flag.Usage = func() {
 		fmt.Printf("Usage of %s:\n", os.Args[0])
-		fmt.Printf("    [-o 5] [-c 127.0.0.1] [-p 5672] [-t mytopic]\n")
+		fmt.Printf("    [-o 5] [-c 127.0.0.1] [-p 5672] [-t mytopic] [-m -1]\n")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
@@ -56,12 +58,18 @@ func main() {
 		log.Fatal("Receiver:", r)
 	}
 
+	numReceived := 0
 	for {
+		if numMessages >= 0 && numReceived >= numMessages {
+			amqpConn.Close(nil)
+			break
+		}
 		if rm, err := r.Receive(); err == nil {
 			rm.Accept()
 			m := rm.Message
 			body := m.Body()
 			fmt.Println(body)
+			numReceived += 1
 		} else if err == electron.Closed {
 			return
 		} else {
